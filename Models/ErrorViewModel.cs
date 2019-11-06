d2a808e5ffe2b6d7de5f44ce339ddbae5bc50970
shivakomuravelly <shivakumarkomuravelly@gmail.com>
2019-11-06 14:42:56 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Beginnerproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
